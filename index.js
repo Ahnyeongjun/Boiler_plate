3ac5f23a58806b8a30a05de147315ff5d6ecee6f
jun <toinin@naver.com>
2021-03-23 11:59:59 +0900

    Add login

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express()
 const port = 5000
 const mongoose = require('mongoose')
 const config = require("./config/key");
+const { User } = require("./model/User")
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
 const connect = mongoose.connect(config.mongoURI,
     {
         useNewUrlParser: true, useUnifiedTopology: true,
@@ -12,13 +16,14 @@ const connect = mongoose.connect(config.mongoURI,
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => res.send('hello_world'))
 
 app.post("/register", (req, res) => {
-
     const user = new User(req.body);
-
     user.save((err, userInfo) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).json({
@@ -27,5 +32,21 @@ app.post("/register", (req, res) => {
     });
 });
 
+app.post('/login', (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) return res.json({
+            loginSuccess: false,
+        })
+        console.log("나 됬음")
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) return res.json({ loginSuccess: false })
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err)
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
+            });
+        });
+    });
+});
 app.listen(port, () => console.log(`Example app port ${port}`))
-
--- model/User.js
+++ model/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -36,19 +37,38 @@ const userSchema = mongoose.Schema({
 userSchema.pre('save', function (next) {
     var user = this;
 
-    if (user.isModified('password'))
-
+    if (user.isModified('password')) {
+        // console.log('password changed')
         bcrypt.genSalt(saltRounds, function (err, salt) {
-            if (err()) return next(err)
+            if (err) return next(err);
 
-            bcrypt.hash(user, passowrd, salt, function (err, hash) {
-                if (err()) return next(err)
-                user.passowrd = hash
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash
                 next()
             })
         })
-    next()
+    } else {
+        next()
+    }
 })
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = { User }
