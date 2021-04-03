8368f817ebd18e6c9b2d864d6da53a3f65aa7453
jun <toinin@naver.com>
2021-04-04 01:28:57 +0900

    ADD app.js

---
+++ server_mysql/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { sequelize } = require('./models');
+const cors = require('cors');
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'))
+app.use(cors());
+
+app.use("/", require("./routes"));
+
+app.set("jwt-secret", process.env.JWT_SECRET);
+app.set("refresh-secret", process.env.REFRESH_SECRET);
+
+sequelize.sync();
+
+app.listen(5000, () => {
+    console.log('server start..');
+});
